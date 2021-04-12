938d6390ab33925fd745c6f472f94e95a308a783
Jairo Flores Z <jhairo4121@gmail.com>
2021-04-12 16:10:03 -0400

    problema2

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+/*Problema 1
+
 var primo = 2;
 var result = true;
 for( var i = 2; i<primo; i++){
@@ -5,4 +7,14 @@ for( var i = 2; i<primo; i++){
         result = false;
     }
 }
-console.log(result);
+console.log(result);*/
+
+function Invertir(cadena : string){
+    var cadenainvertida : String
+    cadenainvertida = ""; 
+    for (var i= cadena.length -1; i>=0 ; i--){
+        cadenainvertida += cadena.charAt(i);
+    }
+    return cadenainvertida;
+} 
+console.log(Invertir("this is a sample string"));
